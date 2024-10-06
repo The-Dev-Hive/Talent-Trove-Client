11069f5c0f9d928018097e448f1677df6e3a0bfe
yeasin2002 <mdkawsarislam2002@gmail.com>
2024-10-06 11:53:41 +0600

    chore: created test route for preview

--- src/app/(front)/page.tsx
+++ src/app/(front)/page.tsx
@@ -1,5 +1,5 @@
-import Text from "@/components/Test/Text";
-import React from "react";
+import Text from '@/app/(front)/Test/Text';
+import React from 'react';
 
 const RootPage = () => {
   return (
