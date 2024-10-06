9d7a6892a5d3cb24c52e894f99d6e326adad4c1c
Rakibul Islam Sumon <learnwithsumon@gmail.com>
2024-10-06 10:10:06 +0600

    adding font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,31 @@
+// app/layout.tsx
 import type { Metadata } from "next";
 import "./globals.css";
+import Navbar from "@/components/Navbar"; // Import Navbar
+import { Inter } from "next/font/google";
 
+export const inter = Inter({
+  weight: ["400", "700"],
+  style: ["normal"],
+  subsets: ["latin"],
+});
+// Export metadata
 export const metadata: Metadata = {
   title: "Talent Trove",
-  description: "Modern  talent management platform",
+  description: "Modern talent management platform",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body className={` antialiased`}>{children}</body>
+      <body className={`antialiased ${inter.className}`}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
