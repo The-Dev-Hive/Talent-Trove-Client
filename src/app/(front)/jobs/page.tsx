199dd3f72890376c9fd5a5bbdab9fe52f9df0fb4
Belal Uddin <coderbelal99@gmail.com>
2024-10-08 14:05:01 +0600

    #6 addded hero and footer

--- src/app/(front)/jobs/page.tsx
+++ src/app/(front)/jobs/page.tsx
@@ -1,9 +1,17 @@
 // reference :  https://preview.cruip.com/job-board/
 
 import React from 'react';
+import Hero from './components/Hero';
+import RootFooter from '@/components/shared/RootFooter';
 
 const JobPage = () => {
-  return <div>JobPage</div>;
+  return (
+    <div>
+      {' '}
+      <Hero />
+      <RootFooter />{' '}
+    </div>
+  );
 };
 
 export default JobPage;
