06a874df3ce756e4b275bd2510481ce4e0bacc55
Belal Uddin <coderbelal99@gmail.com>
2024-10-08 19:01:30 +0600

    #6 added job card and filter

--- src/app/(front)/jobs/page.tsx
+++ src/app/(front)/jobs/page.tsx
@@ -3,12 +3,14 @@
 import React from 'react';
 import Hero from './components/Hero';
 import RootFooter from '@/components/shared/RootFooter';
+import { JobCard } from './components/JobCard';
 
 const JobPage = () => {
   return (
     <div>
       {' '}
       <Hero />
+      <JobCard />
       <RootFooter />{' '}
     </div>
   );
